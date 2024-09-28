ebc35d6489a711ab7e36d30ee49569666190ea72
AdrianGerman <adrian.german1019@gmail.com>
2024-09-28 14:20:57 -0600

    programming chat logic

---
+++ src/public/js/script.js
@@ -0,0 +1,33 @@
+const socket = io()
+
+const send = document.querySelector("#send-message")
+const allMessages = document.querySelector("#all-messages")
+
+send.addEventListener("click", () => {
+  const message = document.querySelector("#message").value
+  socket.emit("message", message)
+})
+
+socket.on("message", ({ user, message }) => {
+  const msg = document.createRange().createContextualFragment(`
+        <div class="message">
+          <div class="image-container">
+            <img
+              src="https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+              alt="me profile image"
+            />
+          </div>
+
+          <div class="message-body">
+            <div class="user-info">
+              <span class="username">${user}</span>
+              <span class="time">Hace 1 segundo</span>
+            </div>
+            <p>
+              ${message}
+            </p>
+          </div>
+        </div
+    `)
+  allMessages.append(msg)
+})
--- src/realtimeServer.js
+++ src/realtimeServer.js
@@ -1,7 +1,13 @@
 module.exports = (httpServer) => {
   const { Server } = require("socket.io")
   const io = new Server(httpServer)
+
   io.on("connection", (socket) => {
-    console.log(socket.io)
+    socket.on("message", (message) => {
+      io.emit("message", {
+        user: "Heim",
+        message
+      })
+    })
   })
 }
